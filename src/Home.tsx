4883861aaa9fec9343937df9d68d5387c7cf1f54
YUKIJUDAI <1787149082@qq.com>
2020-08-21 17:17:33 +0800

    init

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.less";
+
+export default class Home extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return <div>hello wold</div>;
+    }
+}
